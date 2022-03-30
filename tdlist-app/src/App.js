670c3d1fcc03179da60e999f9b321ad119b25ed9
Ryan Pickell <rianchristopher@gmail.com>
2022-03-30 16:07:06 -0700

    added add, edit, delete functionality to app

--- tdlist-app/src/App.js
+++ tdlist-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TdlistsContainer from "./components/TdlistsContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="mainContainer">
+        <div className="topHeading">
+          <h1>A Simple To-Do List App</h1>
+        </div>
+        <TdlistsContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
